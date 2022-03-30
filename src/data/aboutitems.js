ff689ca5fee2f09b5f3ffb45ad7f36a4c951f75f
Danny Tomkins <tomkinsdan@gmail.com>
2022-03-29 20:34:44 -0500

    update resume

--- src/data/aboutitems.js
+++ src/data/aboutitems.js
@@ -3,7 +3,7 @@ import github120img from "../img/github120.png"
 import linkedin256img from "../img/linkedin256.png"
 import email256img from "../img/email256.png"
 import resume256img from "../img/resume256.png"
-import resume from "../img/dtomkins_resume_05-21.pdf"
+import resume from "../img/dtomkins_resume_02-22.pdf"
 
 
 export const aboutItems =
